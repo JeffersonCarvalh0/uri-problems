4989e00e53680187037963926ced2fcfa1fcf7aa
Jefferson <jeffersonhcarvalho@gmail.com>
2018-08-07 22:02:55 -0300

    Added a working solution to divisores

--- sbc-2016/divisores/main.cpp
+++ sbc-2016/divisores/main.cpp
@@ -1,21 +1,31 @@
-// TLE
 # include <iostream>
+# include <cmath>
 
 using namespace std;
 
 int main() {
     ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-    int a, b, c, d, m;
+    int a, b, c, d;
 
     cin >> a >> b >> c >> d;
 
-    m = max(a, b); m = max(m, c); m = max(m, d);
-
-    for (int i = 1; i <= m; ++i) {
+    for (int i = 1; i <= sqrt(c); ++i) {
         if (i % a == 0 && i % b != 0 && c % i == 0 && d % i != 0) {
             cout << i << '\n';
             return 0;
         }
+    }
+
+    int i = c / 2;
+    if (i % a == 0 && i % b != 0 && c % i == 0 && d % i != 0) {
+        cout << i << '\n';
+        return 0;
+    }
+
+    i = c;
+    if (i % a == 0 && i % b != 0 && c % i == 0 && d % i != 0) {
+        cout << i << '\n';
+        return 0;
     }
 
     cout << "-1\n";
