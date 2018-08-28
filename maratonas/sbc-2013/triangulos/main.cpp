6da3f52bf4dd6e4f413b0181b48b6d30e9561e30
Jefferson <jeffersonhcarvalho@gmail.com>
2018-08-28 16:46:16 -0300

    Finally got AC at triangulos

--- maratonas/sbc-2013/triangulos/main.cpp
+++ maratonas/sbc-2013/triangulos/main.cpp
@@ -1,28 +1,23 @@
-// RTE
-
 # include <iostream>
-# include <cstring>
+# include <set>
 
 using namespace std;
 
-// inline int mod(int a, int b) { return (b + (a % b)) % b; }
-
 int main() {
     ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-    bool c[100005];
-    int n, sum, cur, size, ans;
+    set<int> points;
+    int n, sum, cur, arc, ans;
 
     while (cin >> n) {
-        memset(c, 0, sizeof(c));
-        sum = ans = 0; c[0] = true;
-        for (int i = 0; i < n; ++i) cin >> cur, c[sum += cur] = true;
+        points.clear(); sum = ans = 0;
+        while (n--) cin >> cur, points.insert(sum += cur);
 
         if (sum % 3 != 0) cout << "0\n";
         else {
-            size = sum / 3;
-            for (int i = 0; i < sum; ++i)
-                if (c[i] && c[(i + size) % sum] && c[((i + (size * 2)) % sum)])
-                    ++ans, c[(i + size) % sum] = c[((i + (size * 2)) % sum)] = false;
+            arc = sum / 3;
+            for (auto point : points)
+                if (points.find(point + arc) != points.end() &&
+                points.find(point + (arc * 2)) != points.end()) ++ans;
             cout << ans << '\n';
         }
     }
