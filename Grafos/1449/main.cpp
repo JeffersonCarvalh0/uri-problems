ceb8584234c3d2aa1f2d71ed978a7216adea1a9e
Jefferson <jeffersonhcarvalho@gmail.com>
2019-01-27 13:56:41 -0300

    Added 1449

--- Grafos/1449/main.cpp
+++ Grafos/1449/main.cpp
@@ -1,27 +1,29 @@
-# include <iostream>
-# include <cstring>
+# include <iostream>    // Entrada e saída de dados
+# include <cstring>     // memset
 
 using namespace std;
 
 int main() {
-    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0); // Deixar a entrada e saída mais rápidas
+
+    // variáveis
     int t, n, v, a, v1, v2;
     bool adj_mat[55][55];
 
-    cin >> t;
-    while (t--) {
-        int edges = 0;
-        memset(adj_mat, false, sizeof(adj_mat));
-        cin >> n >> v >> a;
-        while (a--) {
-            cin >> v1 >> v2;
-            if (!adj_mat[v1][v2]) {
-                adj_mat[v1][v2] = true;
-                adj_mat[v2][v1] = true;
-                ++edges;
+    cin >> t; // Recebe a quatidade de casos de teste
+    while (t--) { // Enquanto ainda houverem casos de teste
+        int edges = 0; // Número de arestas
+        memset(adj_mat, false, sizeof(adj_mat)); // Coloca falso pra todas as posições da matriz
+        cin >> n >> v >> a; // Recebe o vértice inicial, número de vértices e número de arestas
+        while (a--) { // Enquanto houverem vértices
+            cin >> v1 >> v2; // Recebe os valores dos dois vértices que compõem a aresta
+            if (!adj_mat[v1][v2]) { // Se a aresta NÃO existir
+                adj_mat[v1][v2] = true; // Aresta que vai de v1 à v2 agora existe
+                adj_mat[v2][v1] = true; // Aresta que vai de v2 à v1 agora existe
+                ++edges; // Aumenta 1 na quantidade de arestas
             }
         }
-        cout << edges * 2 << '\n';
+        cout << edges * 2 << '\n'; // Exibe na tela a quantidade de arestas * 2
     }
 
     return 0;
